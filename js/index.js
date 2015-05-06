9b9ab9680bda7b0ab6bc422683bc17825768a36a
giotroni <giorgio@troni.it>
2015-05-06 13:57:55 +0200

    check connection

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ var app = {
   },
   onDeviceReady: function(){
     // ok, il dispositivo è pronto: configuralo
+    app.checkConnection();
     // app.showAlert("Chiamata alla fine del caricamento","msg");
     destinationType=navigator.camera.DestinationType;
     // inizializza l'elenco delle mete e le pagine
@@ -94,6 +95,19 @@ var app = {
   },
   onFail: function(msg){
     showAlert("Foto non riuscita: " + error.code,"msg");
+  },
+  checkConnection: function() {
+    var networkState = navigator.network.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+    dsbMsg('Connection type: ' + states[networkState]);
+    return networkState;
   }
 }
 
